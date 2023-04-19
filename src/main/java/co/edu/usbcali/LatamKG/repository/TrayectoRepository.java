5d74ca7a391b4463cde3b7802dc2e27aea94f17b
GabrielaPuerta <111705848+GabrielaPuerta@users.noreply.github.com>
2023-04-19 11:16:33 -0500

    Delete src/main directory

--- src/main/java/co/edu/usbcali/LatamKG/repository/TrayectoRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbcali.LatamKG.repository;
-
-import co.edu.usbcali.LatamKG.domain.Trayecto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrayectoRepository extends JpaRepository<Trayecto, Integer> {
-}
